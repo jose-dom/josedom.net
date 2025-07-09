be836bd8cbec0a8540493aa66eeb67548a197a89
Jose Dominguez <41589772+jose-dom@users.noreply.github.com>
2025-07-09 14:49:35 -0400

    Add experience filtering on resume page

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,23 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// This file is intentionally blank// Use this file to add JavaScript to your project
+
+document.addEventListener('DOMContentLoaded', function () {
+    const filterButtons = document.querySelectorAll('.filter-btn');
+    const experienceCards = document.querySelectorAll('.experience-card');
+
+    filterButtons.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            const category = btn.getAttribute('data-filter');
+            experienceCards.forEach((card) => {
+                if (card.getAttribute('data-category') === category) {
+                    card.classList.remove('d-none');
+                } else {
+                    card.classList.add('d-none');
+                }
+            });
+        });
+    });
+});
+
